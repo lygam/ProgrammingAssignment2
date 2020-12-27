cba1ffc6672a3ba2064fb7b134270684fe210902
lygam <mcgamban@up.edu.ph>
2020-12-28 01:41:32 +0800

    Modified code in forked assignment to accomplish a programming assignment that returns an inverse matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,29 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Returns the inverse of a given matrix
+## Retrieves inverse from cache if it has already been calculated
 
-## Write a short comment describing this function
+## Creates a matrix object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inversemat <- NULL
+  getmat <- function() x
+  setinverse <- function(inverse) inversemat <<- inverse
+  getinverse <- function() inversemat
+  list(getmat=getmat, setinverse=setinverse, getinverse=getinverse)
 }
 
 
-## Write a short comment describing this function
+## Computes the inverse of matrix returned by above function
+## If the inverse has already been calculated, the cache is retrieved
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  inversemat <- x$getinverse()
+  if(!is.null(inversemat)) {
+    print("Inverse has already been calculated. Retrieving cached data...")
+    return(inversemat)
+  }
+  initialmat <- x$getmatrix()
+  inversemat <- solve(initialmat, ...)
+  x$setinverse(inversemat)
+  inversemat
 }
